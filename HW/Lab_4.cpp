a8bfc79d1c57284d07440b860fd16c414bf7515d
Benlee1000 <60490332+Benlee1000@users.noreply.github.com>
2020-02-19 21:55:21 -0800

    Polished Lab 4 Included functionality to display tables

--- HW/Lab_4.cpp
+++ HW/Lab_4.cpp
@@ -7,17 +7,33 @@
 **********************************    Design    *************************** 
 ** 1.  Define 2D array
 ** 2.  Prompt user for bigrams that start with c and put into array
-** 3.  Call top_bigram and report results. There could be a tie.
-** 4.  Call top_count and report results. There could be a tie.
+** 3.  Display bigram table
+** 4.  Call top_bigram and report results. There could be a tie.
+** 5.  Call top_count and report results. There could be a tie.
 **************************************************************************/  
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 const bool DBG = true;
 const string LETTERS[] = {"a", "b", "c", "\\n"};
 const int NUM_OF_LETTERS = 4;
+
+void show_bigrams_table(int bigrams_[4][4]) {
+    //display header of table
+    cout << "\n\t" << LETTERS[0] << "\t" << LETTERS[1] << "\t" << LETTERS[2] << "\t" << LETTERS[3] << endl;
+    
+    //display letter and values of bigrams in table
+    for (int idx = 0; idx < NUM_OF_LETTERS; idx++) {
+        cout << LETTERS[idx] << "\t" << bigrams_[idx][0] << "\t" << bigrams_[idx][1] << "\t" << bigrams_[idx][2] << "\t" << bigrams_[idx][3] << endl;
+        //display extra space at end of table
+        if (idx == NUM_OF_LETTERS - 1) {
+            cout << "\n";
+        }
+    }
+} //show_bigrams_table
 
 //returns the top bigram, both if tied
 string top_bigram(int bigrams_[4][4]) {
@@ -44,7 +60,7 @@ string top_bigram(int bigrams_[4][4]) {
     return largest_bigram;
 } //top_bigrams
 
-//returns the letter with the most counts, both if tied
+//returns the letter with the most counts, both if tied 
 string top_count(int bigrams_[4][4]) {
     int letter_count[4] = {0, 0, 0, 0};
     int highest = 0;
@@ -111,6 +127,8 @@ int main() {
         bigrams[2][idx] =  user_value;
     } //for
 
+    show_bigrams_table(bigrams);
+
     largest_bigram = top_bigram(bigrams);
     largest_count = top_count(bigrams);
 
@@ -131,12 +149,26 @@ int main() {
 Enter the value for bigram ca: 1
 Enter the value for bigram cb: 7
 Enter the value for bigram cc: 7
+
+        a       b       c       \n
+a       2       4       8       1
+b       0       10      2       0
+c       1       7       7       0
+\n      0       0       0       0
+
 The largest bigram is "bb"
 The most common letters are "a" and "c"
 
 Enter the value for bigram ca: 20
 Enter the value for bigram cb: 20
 Enter the value for bigram cc: 1
+
+        a       b       c       \n
+a       2       4       8       1
+b       0       10      2       0
+c       20      20      1       0
+\n      0       0       0       0
+
 The largest bigrams are "ca" and "cb"
 The most common letter is "c"
 
