df99d199ea63f0fcc2ac36676f551304a71b827b
Benlee1000 <60490332+Benlee1000@users.noreply.github.com>
2020-03-05 16:01:39 -0800

    changed classwork

--- Classwork/pre-processsing.cpp
+++ Classwork/pre-processsing.cpp
@@ -1,6 +1,8 @@
 #include "pre-processing.h"
 
 int main() {
+    int bigrams[64][64] = {0};
+
     // 4 2 1
     // 0 1 1  3 = 1 * 2 + 1 * 1 
     int j = 3;
@@ -11,15 +13,22 @@ int main() {
     // 0 0 1 1
     // 1 0 0 1
 
-    // 0 0 0 0 for bitwise &
+    // 0 0 0 1 for bitwise & (and)
     int g = BitAnd(j,k);
 
+    // 1 0 1 1 for bitwise | (or)
+    int h = j | k;
 
+    // 1 0 1 0 for bitwise ^ (XOR, exclusive or)
+    int i = j ^ k;
 
-
-
+    // 1 1 0 0 for bitwise ~ (inverse)
+    int l =  ~j;
 
 #ifdef DBG
     cout << g << endl;
+    cout << h << endl;
+    cout << i << endl;
+    cout << l << endl;
 #endif
 } //main
