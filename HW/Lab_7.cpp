d7481601649b9352afc9f09ff3c8d47ce65604ab
Benlee1000 <60490332+Benlee1000@users.noreply.github.com>
2020-04-02 16:27:06 -0700

    Started Lab 7

---
+++ HW/Lab_7.cpp
@@ -0,0 +1,81 @@
+/************************************************************************** 
+**************************    Honor Code Affirmation **********************
+**   We, Benjamin Lee and Leighton Yan, have not given or received 
+**   inappropriate help with this assignment. 
+** 
+*************************************************************************** 
+**********************************    Design    *************************** 
+** 1.  
+** 2.
+** 3. 
+**************************************************************************/
+
+/*
+    CSV -- comma separated value. You can export an excel spreadsheet or
+    some other DB into a text file, in which the entries of each row in 
+    the base are seprated by commas.
+
+    Notes:
+    Made set fields to separate string
+    Lines 69,70,72 have errors that I don't know how to fix
+    Waiting for Mr. Pico to give us the data to break up
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class CSV {
+    string line;
+    vector<string> fields;
+    int starting_point = 0;
+    int field_number = 0;
+    
+    public:
+        CSV(string line_) {
+            this->line = line_;
+        } //constuctor
+        vector<string> getFields() {return this->fields;}
+        void setFields() {
+                for (int idx = 0; idx < line.length(); idx++) {
+                    if (line[idx] == ',') {
+                        //if there is a comma, create a substring from start to comma, not including commma
+                        fields.at(field_number) = line.substr(starting_point, idx - starting_point - 1);
+                        //increase field_number for next entry, set new starting point to after comma
+                        field_number++;
+                        starting_point = idx + 1; 
+                    }
+                    else if (idx == line.length() - 1) {
+                        //create one more field for last item in CSV
+                        fields.at(field_number) = line.substr(starting_point, idx - starting_point);
+                    }
+                }
+
+            /*
+            parse this->line into fields
+            e.g. suppose line was: "San Francisco, 123, 5.72"
+            Then setFields would intialize the fields string vector with 
+            3 strings: this->fields.at(0) would be "San Francisco",
+                       this->fields.at(1) would be "123"
+                       this->fields.at(2) would be "5.72"
+            */
+        }
+};
+
+
+int main() {
+    string fromDB = "";
+    CSV csv = new CSV(fromDB);
+    csv->setFields();
+
+    vector<string> f = csv->getFields();
+    cout << f.at(4);
+}
+
+/************************************************************************** 
+************************   Test Report   ********************************** 
+** Worked as designed, as shown by a transcript of program execution:
+
+
+**************************************************************************/ 
