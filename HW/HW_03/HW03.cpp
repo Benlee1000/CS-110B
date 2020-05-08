a044711f2ce63464de438f829c9ec3357e6ace53
Benlee1000 <60490332+Benlee1000@users.noreply.github.com>
2020-05-07 18:59:15 -0700

    Added classwork on part 1

--- HW/HW_03/HW03.cpp
+++ HW/HW_03/HW03.cpp
@@ -464,14 +464,100 @@ bool CSV::update(vector<Directory> filters_, TextList aNames_) {
     return true;
 } //update()
 
+void nextDay(int &m, int &d, int &y) {
+    switch(m) {
+        //30 day months are April, June, September, November
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+        if ( d < 30) { d++; return; }
+        else { d = 1; m++; return; }
+        break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+        //31 days months Jan, Mar, May, Jul, Aug, Oct, Dec
+        if ( d < 31) { d++; return; }
+        else if (m < 12)  { d = 1; m++; return; }
+        else {
+            d = 1;
+            m = 1;
+            y++;
+            return;
+        } //else it's New Years Day
+        break;
+        case 2:
+        //February has 28 days, except if has 29 on leap years.
+        //A leap year is year such that y % 4 = 0 and y %100 != 0
+        // except if y % 400 = 0;
+        if (d < 28) { d++; return;}
+        else if (d == 29) {m++; d =1; return;  }
+        else {
+            if (y % 4 ==0 && (y % 100 != 0 || y % 400 == 0 ) ) { 
+                d++;
+                return;
+            } //if leap year
+            else { d = 1; m++; return; }  //not a leap year
 
+        } //else
+    } //switch
+
+} //nextDay
 //mp HW3 part 1 is to complete the definition of makeNextFileName  
 string makeNextFileName() {
     static string csvFileName = NULL_STRING;    
     //if not initialized, set up the analysis interval and return NULL_STRING
     //if called after being initialized, return the next CSV file in the analysis
     // interval if there is one, or NULL_STRING if there is not.
+    static int mm = -1;
+    static int dd;
+    static int yy;
+    static int numDays;
+    static int curDay;
+  
+    if (mm == -1) {
+        //in your program you will prompt the user for values
+        //  but for now we will set test values
+        string response;
+        while (true) {
+            cout << "Enter month: ";
+            getline(cin,response);
+            try { mm = stoi(response); }
+            catch (exception &e) {
+                cout << "\"" << response << "\" is non-numric" << endl;
+                continue;
+            }
+            break;
+            //if we have not validated the mm is a legal value --
+            //we don't want except valus < 1 or > 12
+        } 
+        dd = 26;
+        yy = 2020;
+        numDays = 8;
+        curDay = 0;
+        return NULL_STRING;
+    } //if we are not initialized
+    else {
+        if (curDay == numDays) {
+            return NULL_STRING;
+        } //if we're done with the analysis interval
+
+    string csvFileName;
+    char cStrBuffer[100];
+
+    //Create a file with the format MM-DD-YYYYY.csv
+    sprintf(cStrBuffer,"%02d-%02d-%04d.csv", mm,  dd,  yy);  
+    csvFileName = cStrBuffer;
+    ++curDay;
+    nextDay(mm,dd,yy);
     
+    return csvFileName;
+  } 
     if (csvFileName == NULL_STRING) {
         csvFileName = "04-30-2020.csv";
         return NULL_STRING;
